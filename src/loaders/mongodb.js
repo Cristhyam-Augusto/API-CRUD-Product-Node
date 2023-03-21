87c08d7bcb0664e01f4ae0ebad6f636ba0fc2651
Cristhyam Augusto <102738785+Cristhyam-Augusto@users.noreply.github.com>
2023-03-21 00:41:53 -0300

    Edit mongodb credencials

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function startDB() {
   await mongoose.connect(
-    "mongodb+srv://CrisAugusto:24061998@cluster0.zqw2vbl.mongodb.net/test"
+    ""
   );
 }
 
